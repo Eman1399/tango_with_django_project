6b5d83ca0d9fc64a0893e6cbfba2da8e9fd2d0e8
Eimantas Bogusis <3015017b@student.gla.ac.uk>
2026-02-08 15:20:55 +0000

    Refactored templates to extend base.html

--- rango/views.py
+++ rango/views.py
@@ -44,9 +44,9 @@ def add_category(request):
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
-    if form.is_valid():
-        form.save(commit = True)
-        return redirect('/rango/')
+        if form.is_valid():
+            form.save(commit = True)
+            return redirect(reverse('rango:index'))
     else:
         print(form.errors)
 
@@ -59,7 +59,7 @@ def add_page(request, category_name_slug):
         category = None
 
     if category is None:
-        return redirect('/rango/')
+        return redirect(reverse('rango:index'))
 
     form = PageForm()
 
